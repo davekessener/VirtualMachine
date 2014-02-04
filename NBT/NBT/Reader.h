b4d5ae3b1c119d6d51a8fdc00f4ebd420c556ad5
BroSkylark <BroSkylark@gmail.com>
2014-02-04 12:12:11 +0100

    Regular update.

---
+++ NBT/NBT/Reader.h
@@ -0,0 +1,68 @@
+#ifndef NBT_READER_H
+#define NBT_READER_H
+
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include "NBT.h"
+#include "gzstream.h"
+
+namespace NBT
+{
+	class basic_nbt_reader
+	{
+		public:
+			template<typename T>
+			T read() const;
+			void read(BYTE *d, size_t n) const
+			{
+				do_read(static_cast<char *>(d), n);
+			}
+		protected:
+			virtual void do_read(char *, size_t) const = 0;
+	};
+
+	template<typename T>
+	class nbt_reader : public basic_nbt_reader
+	{
+		public:
+			nbt_reader(T& t) : is(t) { }
+		protected:
+			void do_read(char *, size_t) const;
+		private:
+			T &is;
+	};
+
+// # ---------------------------------------------------------------------------
+
+	template<typename T>
+	T basic_nbt_reader::read(void) const
+	{
+		T t;
+		do_read(static_cast<char *>(&t), sizeof(T));
+
+#ifndef RETAIN_ENDIANNESS
+		std::reverse(static_cast<BYTE *>(&t), static_cast<BYTE *>(&t) + sizeof(T));
+#endif
+
+		return t;
+	}
+
+// # ---------------------------------------------------------------------------
+
+#ifndef READER_IMP
+	extern template class nbt_reader<std::istream>;
+	extern template class nbt_reader<gzip::igzstream>;
+#endif
+
+	typedef nbt_reader<std::istream> nbt_std_reader;
+	typedef nbt_reader<gzip::igzstream> nbt_gzip_reader;
+
+	typedef basic_nbt_reader nbtistream;
+}
+
+#endif
+
---
+++ NBT/NBT/common.h
@@ -0,0 +1,55 @@
+#ifndef NBT_
+#define NBT_
+
+#include <cstdint>
+#include <memory>
+
+namespace NBT
+{
+	typedef std::uint8_t   BYTE;
+	typedef std::uint16_t  WORD;
+	typedef std::uint32_t DWORD;
+	typedef std::uint64_t QWORD;
+
+// # ---------------------------------------------------------------------------
+
+	class NBTBase;
+	template<BYTE, typename> class NBTSimple;
+	template<BYTE, typename, typename> class NBTArray;
+	template<BYTE> class NBTString;
+	template<BYTE> class NBTList;
+	template<BYTE> class NBTTagCompound;
+
+// # ---------------------------------------------------------------------------
+
+	typedef NBTSimple		<1, BYTE> 			TAG_Byte;
+	typedef NBTSimple		<2, WORD> 			TAG_Short;
+	typedef NBTSimple		<3, DWORD> 			TAG_Int;
+	typedef NBTSimple		<4, QWORD> 			TAG_Long;
+	typedef NBTSimple		<5, float>	 		TAG_Float;
+	typedef NBTSimple		<6, double> 		TAG_Double;
+	typedef NBTArray		<7, DWORD, BYTE> 	TAG_Byte_Array;
+	typedef NBTString		<8> 				TAG_String;
+	typedef NBTList			<9> 				TAG_List;
+	typedef NBTTagCompound	<10> 				TAG_Compound;
+	typedef NBTArray		<11, DWORD, DWORD> 	TAG_Int_Array;
+
+// # ---------------------------------------------------------------------------
+
+	typedef std::shared_ptr<NBTBase> NBT_ptr_t;
+
+	typedef std::shared_ptr<TAG_Byte>		TAG_Byte_ptr_t;
+	typedef std::shared_ptr<TAG_Short>		TAG_Short_ptr_t;
+	typedef std::shared_ptr<TAG_Int>		TAG_Int_ptr_t;
+	typedef std::shared_ptr<TAG_Long>		TAG_Long_ptr_t;
+	typedef std::shared_ptr<TAG_Float>		TAG_Float_ptr_t;
+	typedef std::shared_ptr<TAG_Double>		TAG_Double_ptr_t;
+	typedef std::shared_ptr<TAG_Byte_Array>	TAG_Byte_Array_ptr_t;
+	typedef std::shared_ptr<TAG_String>		TAG_String_ptr_t;
+	typedef std::shared_ptr<TAG_List>		TAG_List_ptr_t;
+	typedef std::shared_ptr<TAG_Compound>	TAG_Compound_ptr_t;
+	typedef std::shared_ptr<TAG_Int_Array>	TAG_Int_Array_ptr_t;
+}
+
+#endif
+
