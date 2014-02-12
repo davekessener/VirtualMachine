16ad0885f6f196d8c2f2903b5a5a42acffe18b73
BroSkylark <BroSkylark@gmail.com>
2014-02-12 22:19:33 +0100

    Regular update.

--- NBT/test/test.cc
+++ NBT/test/test.cc
@@ -1,194 +1,204 @@
 #include <stdio.h>
-#include <cstring>
 #include <stdlib.h>
-#include <iostream>
-#include <vector>
-#include <mutex>
-#include <future>
-#include <thread>
+#include <string.h>
 #include <ncurses.h>
+#include <iostream>
 #include "Timer.h"
 
 using namespace std;
 
-mutex mtx;
-
-void ncurses_init(void)
+class Banner
 {
-	mtx.lock();
-	initscr();
-	keypad(stdscr, true);
-	cbreak();
-	curs_set(0);
-	timeout(10);
-	mtx.unlock();
-}
-
-void ncurses_drawAtXY(int x, int y, const string& s)
-{
-	mtx.lock();
-
-	int _x, _y;
-	getyx(stdscr, _y, _x);
-
-	move(y, x);
-
-	addstr(s.c_str());
-
-	move(_y, _x);
-
-	refresh();
-
-	mtx.unlock();
-}
-
-void ncurses_readline(char *buf, bool &done)
-{
-	int x, y, i = 0;
-	int ch;
-	Timer t;
-
-	mtx.lock();
-	move(0, 0);
-	addstr(" ");
-	getyx(stdscr, y, x);
-	move(42, 0);
-	addstr(":");
-	curs_set(1);
-	mtx.unlock();
-
-	while(1)
-	{
-		mtx.lock();
-		echo();
-		timeout(1);
-		ch = getch();
-		timeout(5);
-		noecho();
-		mtx.unlock();
-
-		t.sleep(1);
-
-		switch(ch)
+	public:
+		Banner(const string& s, int x, int y) : _msg(s + "    "), _x(x), _y(y), _pos(0), _t(0) { }
+		~Banner( ) { }
+		void draw(void)
 		{
-			case ERR:
-				continue;
-			case '\n':
-				goto end;
-			default:
-				if(	ch >= 'a' && ch <= 'z' || 
-					ch >= 'A' && ch <= 'Z' || 
-					ch >= '0' && ch <= '9' || ch == ' ' || ch == '\'' || ch == ':') buf[i++] = (char) ch;
+			static int _o_pos = -1;
+
+			if(_o_pos == _pos) return;
+
+			_o_pos = _pos;
+
+			string s = (_msg + _msg).substr(_pos, _msg.length());
+
+			toScreen(s.c_str());
 		}
+		void toScreen(const char *s)
+		{
+			int x, y;
+
+			getyx(stdscr, y, x);
+			move(_y, _x);
+			addstr(s);
+			move(y, x);
+		}
+		void update(int ms)
+		{
+			_t += ms;
+			while(_t > 200)
+			{
+				_t -= 200;
+				if(++_pos == _msg.length()) _pos = 0;
+			}
+		}
+		void setMsg(const string& s)
+		{
+			if(s.length() == 0) return;
+
+			char *tmp = strdup(_msg.c_str());
+			memset(tmp, ' ', _msg.length());
+			toScreen(tmp);
+			free(tmp);
+
+			_msg = s + "    ";
+			_pos = 0;
+		}
+
+	private:
+		string _msg;
+		int _x, _y, _t, _pos;
+};
+
+inline int isIn(int c, const char *s) { while(*s) if(c == *s++) return 1; return 0; }
+
+int readAline(char *buf)
+{
+	static int i = -1, idx;
+	static int x, y;
+
+	if(i == -1)
+	{
+		i = 0;
+		idx = 0;
+		getyx(stdscr, y, x);
+		curs_set(1);
+		if(int l = strlen(buf)) memset(buf, '\0', l);
 	}
 
+	move(42, 0);
+	clrtoeol();
+	addstr(":");
+	addstr(buf);
+	move(42, idx + 1);
+
+	int ch = getch();
+	int l = strlen(buf);
+
+	const char *el = " !*()-=+_;:'\"/?.>,<[]{}\\|@#$%^&`~";
+
+	if(ch == ERR) return 0;
+
+	switch(ch)
+	{
+		case KEY_LEFT:
+			if(idx > 0) --idx;
+			ch = 0;
+			break;
+		case KEY_RIGHT:
+			if(idx < i) ++idx;
+			ch = 0;
+			break;
+		case KEY_UP:
+			idx = i;
+			ch = 0;
+			break;
+		case KEY_DOWN:
+			idx = 0;
+			ch = 0;
+			break;
+		case KEY_BACKSPACE:
+		case 0x7f:
+			if(idx > 0)
+			{
+				int _i = idx - 1;
+				while(_i++ < i) buf[_i - 1] = buf[_i];
+				--idx;
+				--i;
+			}
+			ch = 0;
+			break;
+		case '\e':
+			buf[0] = '\0';
+			goto end;
+	}
+
+	if(ch == '\n')
+	{
+		goto end;
+	}
+	else if(ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z' || ch >= '0' && ch <= '9' || isIn(ch, el))
+	{
+		int _i = i;
+		while(_i > idx) buf[_i] = buf[--_i];
+		buf[idx++] = (char) ch;
+		i++;
+		addch((char)ch);
+	}
+
+	return 0;
+
 end:
-
-	mtx.lock();
-
-	buf[i] = '\0';
-	done = true;
+	
+	i = -1;
+	curs_set(0);
 	move(42, 0);
 	clrtoeol();
 	move(y, x);
-	curs_set(0);
-	refresh();
 
-	mtx.unlock();
-
-	return;
+	return 1;
 }
-
-void ncurses_refresh(void)
-{
-	mtx.lock();
-	refresh();
-	mtx.unlock();
-}
-
-struct Banner
-{
-	Banner(const string& s, int x, int y) : _msg(s + "    "), _x(x), _y(y), _pos(0), _t() { }
-	void update( )
-	{
-		if(_t.elapsed() > 200)
-		{
-			_t.reset();
-			if(++_pos == _msg.length()) _pos = 0;
-		}
-	}
-
-	void draw( )
-	{
-		string s(_msg + _msg);
-
-		ncurses_drawAtXY(_x, _y, s.substr(_pos, _msg.length()));
-	}
-
-	void setMsg(const string& s)
-	{
-		if(s.length() == 0) return;
-		char *tmp = strdup(_msg.c_str());
-		memset(tmp, ' ', _msg.length());
-		_msg = string(tmp);
-		draw();
-		free(tmp);
-		_msg = s + "    ";
-		_pos = 0;
-	}
-
-	string _msg;
-	int _x, _y;
-	int _pos;
-	Timer _t;
-};
 
 int main(void)
 {
-	Banner b(string("Hi there, handsome."), 10, 3);
-	Timer timer;
+	Banner b("Hello, world!", 10, 3);
+	Timer t;
 
-	ncurses_init();
+	initscr();
+	keypad(stdscr, true);
+	noecho();
+	curs_set(0);
+	timeout(0);
+	clear();
+	refresh();
 
-	future<void> f;
-	bool done = false, running = false;
-	char buf[256];
+	char buffer[256];
+	bool querying = false;
+
+	memset(buffer, '\0', 256);
 
 	int ch;
-	while(true)
+	while(1)
 	{
-		if(running)
-		{
-			timer.sleep(10);
-		}
-		else
-		{
-			mtx.lock();
-			ch = getch();
-			mtx.unlock();
-		}
+		int d = t.elapsed();
+		t.reset();
 
-		if(ch == KEY_F(1)) break;
+		if(!querying) ch = getch();
 
-		if(done)
-		{
-			f.get();
-			b.setMsg(string(buf));
-			done = false;
-			running = false;
-		}
-
-		b.update();
+		b.update(d);
 		b.draw();
 
-		ncurses_refresh();
+		if(ch == 'q') break;
 
-		if(ch == ':' && !running)
+		if(ch == ':' && !querying)
 		{
-			running = true;
-			f = async(launch::async, [&buf, &done]()->void { ncurses_readline(buf, done); });
 			ch = 0;
+			querying = true;
 		}
+
+		if(querying)
+		{
+			if(readAline(buffer))
+			{
+				querying = false;
+				b.setMsg(buffer);
+			}
+		}
+
+		refresh();
+
+		d = 16 - (d + t.elapsed());
+		if(d > 0) t.sleep(d);
 	}
 
 	endwin();
