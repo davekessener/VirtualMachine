0d9bbc5a7cd7afe76f2d6ec75341f3cb87c2b520
davekessener <davekessener@gmail.com>
2015-04-23 11:53:49 +0200

    Regular update.

--- RND/AF/main.cc
+++ RND/AF/main.cc
@@ -11,57 +11,51 @@ try
 
 	using namespace AF::DEA;
 
-	Builder makedea;
-
-	makedea.addState("0");
-	makedea.addState("1");
-	makedea.addState("2");
-	makedea.addState("3");
-	makedea.addState("4");
-	makedea.addState("5");
-	makedea.addState("X");
-	makedea.addSymbol("V");
-	makedea.addSymbol("E");
-	makedea.addSymbol("T");
-	makedea.addSymbol("X");
-	makedea.addSymbol("S");
-	makedea.addRelation("0", "V", "1");
-	makedea.addRelation("0", "E", "2");
-	makedea.addRelation("0", "T", "3");
-	makedea.addRelation("1", "E", "2");
-	makedea.addRelation("2", "T", "3");
-	makedea.addRelation("3", "X", "4");
-	makedea.addRelation("3", "S", "5");
-	makedea.addRelation("4", "S", "5");
-	makedea.fillDelta("X");
-	makedea.setInitialState("0");
-	makedea.makeStateAccepting("5");
-
-	auto dea = makedea.generate();
-
-	dea = DEA::Read(std::cin);
-
-	auto test = [&dea](const char *s)
-	{
-		std::string b(s);
-		std::vector<std::string> v;
-
-		while(*s)
-		{
-			std::string t;
-			t.push_back(*s);
-			v.emplace_back(std::move(t));
-			++s;
-		}
-
-		std::cout << "'" << b << "' " << (dea.accepts(v.cbegin(), v.cend()) ? "T" : "F") << std::endl;
-	};
+	auto dea = DEA::Read(std::cin);
 
 	auto tbl = dea.generateTable();
 
+	std::vector<std::set<State>> eqs;
 	for(const auto& e : tbl)
 	{
-		if(!e.second) std::cout << e.first.first << ", " << e.first.second << std::endl;
+		if(!e.second)
+		{
+			bool d = false;
+			for(auto& o : eqs)
+			{
+				if(o.find(e.first.first) != o.end())
+				{
+					o.insert(e.first.second);
+					d = true;
+					break;
+				}
+				else if(o.find(e.first.second) != o.end())
+				{
+					o.insert(e.first.first);
+					d = true;
+					break;
+				}
+			}
+			if(!d)
+			{
+				std::set<State> t;
+				t.insert(e.first.first);
+				t.insert(e.first.second);
+				eqs.emplace_back(std::move(t));
+			}
+		}
+	}
+
+	for(const auto& eq : eqs)
+	{
+		bool f = true;
+		for(const auto& e : eq)
+		{
+			if(!f) std::cout << ", ";
+			std::cout << e;
+			f = false;
+		}
+		std::cout << std::endl;
 	}
 
 	return 0;
